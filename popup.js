04b3f7f1db1fc6bb759dd98db5f68ca6c23a1090
unknown <abijithmv01@gmail.com>
2022-10-27 19:21:06 +0530

    Added set

--- popup.js
+++ popup.js
@@ -1,25 +1,20 @@
 document.addEventListener("DOMContentLoaded", ()=> {
 
-document.getElementById('save').addEventListener("click", AddCommand);    
+document.getElementById('save').addEventListener("click", AddCommand);
+document.getElementById('get').addEventListener("click", AddCommand);   
 })
 
 function AddCommand(){
     var com = document.getElementById("command").value;
     var snpt = document.querySelector('#snippet').value;
-    alert(com);
     let savedCommands = {
         command : com,
         snippet: snpt
     }
-    alert(savedCommands);
-    let savedData;
-    if(localStorage.getItem('data' === null)){
-        savedData = [savedCommands]
-    }
-    else{
-        savedData = JSON.parse(localStorage.getItem('data'));
-        savedData.push(savedCommands);
-        localStorage.setItem('data', JSON.stringify(savedData));
-    }
+    chrome.storage.local.set({'Command': savedCommands}, function() {
+        alert('Success');
+      });
+      
     location.href = "popup.html";
 }
+

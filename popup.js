06c5facae5be23e1fc54b1dff8701079c9cb2054
abelzach <abelsimonz@gmail.com>
2022-10-27 15:00:31 +0530

    More modifications

--- popup.js
+++ popup.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", ()=> {
+
+document.getElementById("Button").addEventListener("click", AddCommand);    
     function AddCommand(){
         var command = document.getElementById("command").value;
         var snippet = document.getElementById("snippet").value;
+        console.log(snippet.value);
         submitOK = true;
         if(snippet.value < 0)
         {
@@ -14,7 +17,7 @@ document.addEventListener("DOMContentLoaded", ()=> {
             submitOK = false;
         }
 
-        if(submitOK == TRUE){
+        if(submitOK == true){
             chrome.storage.local.set({command: snippet}, () => {
                 if (chrome.runtime.lastError)
                     console.log('Error setting');

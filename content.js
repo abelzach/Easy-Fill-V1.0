cd79bd21049d45dd6dc7d70fb3d203ef5a747f43
abelzach <abelsimonz@gmail.com>
2022-10-28 00:57:49 +0530

    regex working

--- content.js
+++ content.js
@@ -1,11 +1,19 @@
 window.onload = () => {
 document.body.addEventListener("click", getFocused);
 }
+var element1;
 
 function getFocused () {
     const element = document.activeElement.tagName;
     if(element === "INPUT" || element === "DIV"){
-        const element1 = document.activeElement.id;
-        alert(element1);
+        element1 = document.activeElement.id;
     }
+    Replace();
+}
+
+function Replace(){
+    regexp = new RegExp("/add", "g");
+    var v = document.getElementById(element1).textContent;
+    v = v.replace(regexp, "node_address");
+    document.getElementById(element1).textContent = v;
 }
